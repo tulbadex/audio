03b1d6f927fc8f93b2d49afc49e50e067ecf9350
ibrahim <ibrahimadedayo@rocketmail.com>
2021-08-26 08:09:36 -0700

    Added auth

--- index.js
+++ index.js
@@ -1,1 +1,91 @@
-console.log('Am here')
+const express = require('express');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const jwt = require('jsonwebtoken');
+const { send } = require('process');
+
+const app = express();
+
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { v4: uuidV4 } = require('uuid')
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+require('./routes/auth')(app);
+// require('./app/routes/user.routes')(app);
+
+const db = require("./models");
+const Role = db.role;
+
+db.sequelize.sync({ force: true }).then(() => {
+    console.log('Drop and Resync Db');
+    initial();
+});
+
+function initial() {
+    Role.create({
+        id: 1,
+        name: "user"
+    });
+
+    Role.create({
+        id: 2,
+        name: "moderator"
+    });
+
+    Role.create({
+        id: 3,
+        name: "admin"
+    });
+}
+
+
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('join-room', (roomId, userId) => {
+        console.log('a user connected' + roomId + ' ' + userId);
+        socket.join(roomId)
+        socket.to(roomId).broadcast.emit('user-connected', userId)
+    })
+});
+
+function verifyToken(req, res, next) {
+    // Get the auth header value
+    const bearerHeader = req.headers['authorization']
+    // Token Format = Bearer <token>
+    if (typeof bearerHeader !== "undefined" || typeof bearerHeader !== null) {
+        const bearer = bearerHeader.split(' ');
+        // get token
+        const bearerToken = bearer[1]
+        req.token = bearerToken;
+        next();
+    }else{
+        res.sendStatus(403)
+    }
+}
+
+
+/* server.listen(3000, () => {
+    console.log('listening on *:3000');
+}); */
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
